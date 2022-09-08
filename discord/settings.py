d66094485bc823b924b350dd02b705c7a9191735
Vinicius <viniciusvieiradearaujo@hotmail.com>
2022-09-07 22:56:26 -0300

    heroku configs 4

--- discord/settings.py
+++ discord/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 MEDIA_URL = '/images/'
 
@@ -150,14 +150,12 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_URL = "/static/"
+# django_heroku.settings(locals())
 
-
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = "/static/"
-django_heroku.settings(locals())
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
