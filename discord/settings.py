a4382725d03219b0bb5b63c023b3a94584db429d
Vinicius <viniciusvieiradearaujo@hotmail.com>
2022-10-02 18:15:14 -0300

    config

--- discord/settings.py
+++ discord/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--h5u385p^a6tqnbr-x06$lkb(2+=g%ey0s305s%+0amo*and99'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
